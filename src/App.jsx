b9a3a7817e8cb48a73d1be8154a6feda035d308d
Tim Hart <normanclanger@gmail.com>
2024-12-18 09:34:56 +0000

    Fetch functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,92 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  var count;
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+	<h1>Tim's Train Departure Board</h1>
+	<p></p>
       </div>
-      <h1>Vite + React</h1>
+
+      <div>
+	Enter 3 letter station code&nbsp;&nbsp;   
+	<input type="text" id="myInput" />
+	<p></p>
+	<a href="https://www.nationalrail.co.uk/stations/">Look up station codes</a>
+	<p></p>Error checking of station codes not implemented - an incorrect station code will result in the unit rebooting until a recognised code is sent
+      </div>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => process((document.getElementById("myInput")).value)}>
+          Change Station
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="status">
+	<label id="statusmessage"></label>
+      </div>
+
+      <div className="check">
+	<label id="question"></label> <p></p>
+	<input type="text" id="coursing"/>
+      </div>
+
+      <div className="course">
+        <button name="coursesubmit" onClick={() => checkcourse((document.getElementById("coursing")).value)}>
+          Submit
+        </button>
+      </div>
+
+      <div className="result">
+	<label id="final"></label>
+      </div>
+
+
     </>
   )
 }
 
+function process(v)
+{
+	console.log(v)
+	if (v.length!=3)
+	  document.getElementById("statusmessage").innerText="Not 3 letters";
+	else
+	{
+          document.getElementById("statusmessage").innerText="Captcha for bellringers";
+	  document.getElementById("question").innerText="Coursing order 53246.  Bob at home.  What's the new coursing order?";	
+	}
+	
+}
+
+function checkcourse(v)
+{
+	if ((document.getElementById("myInput")).value.length!=3)
+	{
+		document.getElementById("statusmessage").innerText=document.getElementById("myInput").value+" is not 3 letters";
+		document.getElementById("question").innerText="";
+		document.getElementById("final").innerText="";
+	}
+
+	if (document.getElementById("myInput").value.length==3)
+	{
+
+	  if (v=="52436")
+	  {
+		document.getElementById("final").innerText="Updating to station code "+document.getElementById("myInput").value;
+
+fetch("https://api.balena-cloud.com/v7/device_environment_variable(62657750)", {  method: "PATCH",  headers: {    "Content-type": "application/json" , "Authorization": "Bearer JZvFs0Q4Pc6TFxny8MP6l7RpW13YBbCd"  },  body: JSON.stringify({    value: document.getElementById("myInput").value  })}) .then(response => {    console.log(response.status); })
+
+
+	  }
+	  else
+		document.getElementById("final").innerText="Oops miscall - Early beer";
+	}
+}
+
+
 export default App
+
